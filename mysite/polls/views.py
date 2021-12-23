8ebb70d912ed87687be631df2e15a2eb900a56a9
ankoi2389 <thanhan2389@gmail.com>
2021-12-23 10:22:37 +0700

    first commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, response
+
+# Create your views here.
+def index(request):
+    response =HttpResponse()
+    response.writelines("<h1>Xin chào</h1>")
+    response.write("Đây là app polls")
+    return response
