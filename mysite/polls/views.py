8e32f5c92ce9c88f57ece801521c8a24fbd93f1f
Ngo Tuan Dung <zun1903@gmail.com>
2021-12-27 17:02:57 +0700

    test commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,3 +23,6 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output) 
+
+def test_view(request):
+    return 'Hello internet'
