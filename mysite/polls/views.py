2250fc5391828776f0f863a27953ed7a737dc23b
ankoi2389 <thanhan2389@gmail.com>
2021-12-29 15:04:01 +0700

    Message

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,8 +12,7 @@ def results(request, question_id):
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
+    context = {'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
 
