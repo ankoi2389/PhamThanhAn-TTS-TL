87f5db7a64b3079ce79bace1ff3181cc45eebb86
ankoi2389 <thanhan2389@gmail.com>
2021-12-29 15:11:39 +0700

    Message

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse, response
+from django.http import HttpResponse,Http404
+
+from django.template import loader
+from django.shortcuts import render,get_object_or_404
 from .models import Question
 
-# Create your views here.
-# def index(request):
-#     response = HttpResponse()
-#     response.writelines("<h1>Xin chào</h1>")
-#     response.write("Đây là app polls")
-#     return response
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,}
+    return HttpResponse(template.render(context, request))
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
