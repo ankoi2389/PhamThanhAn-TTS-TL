63d159a26336592fd14d41107a5b0a1ce23a504c
ankoi2389 <thanhan2389@gmail.com>
2021-12-29 14:55:55 +0700

    Message

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,8 @@
-# from django import urls
 from django.urls import path
+
 from . import views
 
-# urlpatterns=[
-#     path('',views.index)
-# ]
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
